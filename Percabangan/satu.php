f706e96e136325cc78e9ab615ce6ce89c7d26c58
aratane <akunbotak503@gmail.com>
2022-02-28 03:36:31 +0700

    b

--- Percabangan/satu.php
+++ Percabangan/satu.php
@@ -48,6 +48,47 @@
         default:
             echo "Bukan sudut lancip, tumpul, maupun lurus";
     }
+
+    // perintah for
+    for ($x = 1; $x <= 100; $x++) {
+        echo "Angka $x <br>";
+    }
+
+    // perintah while
+    $i = 1;
+    while ($i <= 6) {
+        echo "<h$i>Ini adalah contoh heading $i <h$i>";
+        $i++;
+    }
+
+    // perintah do-while
+    $i = 1;
+    do {
+        echo "ini perulangan yg ke - $i <br>";
+        $i++;
+    } while ($i <= 6);
+
+    // perintah foreach
+    $a = array("Surakarta", "Jakarta", "Yogyakarta");
+    foreach ($a as $data) {
+        echo "$data <br>";
+    }
+
+
+    // fungsi pada php
+    function calculate()
+    {
+        $a = 75;
+        $b = 5;
+        echo $a / $b;
+    }
+    calculate();
+
+    function calculate2($a, $b, $c = 10)
+    {
+        echo $a / $b - $c;
+    }
+    calculate2(75, 5);
     ?>
 </body>
 
